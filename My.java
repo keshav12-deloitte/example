abcc6d67be115f074457560fc2c1e569f0babd44
Chander <101085487+keshav12-deloitte@users.noreply.github.com>
2022-03-16 10:40:22 +0530

    first-commit

---
+++ My.java
@@ -0,0 +1,9 @@
+public class My {
+    public static void main(String[] args) {
+        Student obj = new Student();
+        obj.roll_no=23;
+        obj.name="keshav";
+        System.out.println(obj.roll_no);
+        System.out.println(obj.name);
+    }
+}
